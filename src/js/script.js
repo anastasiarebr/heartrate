2db4905e19c9c3bc2710cd6355e2e80ac4b744c5
Anastasia <anastasiarebr@gmail.com>
2020-09-18 12:38:49 +0300

    tabs js

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+    //carousel
     $('.carousel__inner').slick(
         {
            
@@ -17,4 +19,25 @@ $(document).ready(function(){
             ]
         }
     );
+
+    //tabs
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+        });
+
+        //подробнее и назад в табах
+    function toggleSlide(item){
+        $(item).each(function(i){
+            $(this).on('click', function(e){
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        })
+    };
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
   });
