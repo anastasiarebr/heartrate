1bee23245d91fc7dcb29d7cb0e88586766d9883f
Anastasia <anastasiarebr@gmail.com>
2020-09-22 16:13:12 +0300

    scroll

--- src/js/script.js
+++ src/js/script.js
@@ -104,4 +104,22 @@ $(document).ready(function(){
         });
         return false;
     });
-  });
+
+    // появление стрелочки и плавный скролл
+
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 1600){
+            $('.pageup').fadeIn();
+        } else{
+            $('.pageup').fadeOut();
+        }
+
+    });
+
+    $("a[href^='#']").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
+});
